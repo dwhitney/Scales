826126b03ce37963fc60b98d571c53be580cb68e
molecule <molecule@atom.(none)>
2009-11-23 12:02:24 -0500

    made it so all libs are included in a build

--- core/project/build/ScalesCoreProject.scala
+++ core/project/build/ScalesCoreProject.scala
@@ -2,9 +2,9 @@ import sbt._
 
 class ScalesCoreProject(info: ProjectInfo) extends DefaultProject(info){
 
-	val scalaTest = "org.scalatest" % "scalatest" % "0.9.5" % "test"
-	val mockito = "org.mockito" % "mockito-all" % "1.8.0" % "test"
-	val servlet = "javax.servlet" % "servlet-api" % "2.5" % "provided"
+	val scalaTest = "org.scalatest" % "scalatest" % "1.0"
+	val mockito = "org.mockito" % "mockito-all" % "1.8.0"
+	val servlet = "javax.servlet" % "servlet-api" % "2.5"
 
 	val repo1 = "maven-repo" at "http://repo2.maven.org/maven2/"
 	
--- sitemesh/project/build/SiteMeshProject.scala
+++ sitemesh/project/build/SiteMeshProject.scala
@@ -2,7 +2,7 @@ import sbt._
 
 class SiteMeshProject(info: ProjectInfo) extends DefaultProject(info){
 	
-	val jasper = "org.apache.tomcat" % "jasper" % "6.0.20" % "provided"
-	val sitemesh = "opensymphony" % "sitemesh" % "2.4.2" % "provided"
+	val jasper = "org.apache.tomcat" % "jasper" % "6.0.20"
+	val sitemesh = "opensymphony" % "sitemesh" % "2.4.2"
 	
 }
